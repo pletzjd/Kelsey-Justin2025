bd0cf599f6c9fd1186f84cafd2aea02a46184bd9
pletzjd <j.pletzer17@gmail.com>
2024-04-28 10:28:11 -0400

    RSVP form now submits to a google sheets

--- assets/script/index.js
+++ assets/script/index.js
@@ -7,3 +7,18 @@ plusOne.addEventListener('click', () => {
         guest.setAttribute('hidden','true')
     }
 })
+
+const form = document.getElementById('rsvpForm');
+form.addEventListener("submit", function(e) {
+  e.preventDefault();
+  const data = new FormData(form);
+  const action = e.target.action;
+  fetch(action, {
+    method: 'POST',
+    body: data,
+  })
+  .then(() => {
+    alert("Success!");
+    form.reset()
+  })
+});
